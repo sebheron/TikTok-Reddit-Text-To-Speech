82256d95a9e9fd7438cf1cbcd7e3153eba67c33d
Seb Heron <sebheron21@hotmail.co.uk>
2022-05-30 19:25:15 +0100

    Add video service interface

---
+++ RedditTextToSpeech.Logic/Services/IVideoService.cs
@@ -0,0 +1,14 @@
+﻿using RedditTextToSpeech.Core;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RedditTextToSpeech.Logic.Services
+{
+    public interface IVideoService
+    {
+        Task<string> GetVideo(string path, IList<AudioImagePair> values, TimeSpan startTime, string background);
+
+        string Extension { get; }
+    }
+}
