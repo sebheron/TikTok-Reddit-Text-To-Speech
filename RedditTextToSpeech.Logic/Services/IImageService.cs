635afd531869e6a2c939284f16c9e1af75866dfd
Seb Heron <sebheron21@hotmail.co.uk>
2022-05-30 12:46:41 +0100

    Add image service interface

---
+++ RedditTextToSpeech.Logic/Services/IImageService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedditTextToSpeech.Logic.Services
+{
+    public interface IImageService
+    {
+        Task<string> GetImage(string path, string title, string username, string subreddit);
+        Task<string> GetImage(string path, string text);
+    }
+}
