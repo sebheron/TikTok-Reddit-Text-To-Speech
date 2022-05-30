b3f3e50601c44ec07bf74c54cbe79df20a2b1a8d
Seb Heron <sebheron21@hotmail.co.uk>
2022-05-30 19:21:44 +0100

    Add core items

---
+++ RedditTextToSpeech.Core/AudioImagePair.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RedditTextToSpeech.Core
+{
+    public class AudioImagePair
+    {
+        /// <summary>
+        /// Creates an instance of audio image pair, used to collate audio and image paths.
+        /// </summary>
+        /// <param name="audioPath">The path to the audio file.</param>
+        /// <param name="imagePath">The path to the image file.</param>
+        public AudioImagePair(string audioPath, string imagePath)
+        {
+            this.AudioPath = audioPath;
+            this.ImagePath = imagePath;
+        }
+
+        /// <summary>
+        /// The path to the audio file.
+        /// </summary>
+        public string AudioPath { get; }
+
+        /// <summary>
+        /// The path to the image file.
+        /// </summary>
+        public string ImagePath { get; }
+    }
+}
