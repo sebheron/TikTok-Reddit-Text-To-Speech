971184a03d2603f6b1c15caec5bb092aa639f5c8
Seb Heron <sebheron21@hotmail.co.uk>
2022-05-30 05:30:30 +0100

    Implement azure authentication

---
+++ RedditTextToSpeech.Logic/Services/AzureAuthenticationService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedditTextToSpeech.Logic.Services
+{
+    public class AzureAuthenticationService : IAzureAuthenticationService
+    {
+        private string subscriptionKey;
+        private string tokenFetchUri;
+
+        public AzureAuthenticationService(string tokenFetchUri, string subscriptionKey)
+        {
+            if (string.IsNullOrWhiteSpace(tokenFetchUri))
+            {
+                throw new ArgumentNullException(nameof(tokenFetchUri));
+            }
+            if (string.IsNullOrWhiteSpace(subscriptionKey))
+            {
+                throw new ArgumentNullException(nameof(subscriptionKey));
+            }
+            this.tokenFetchUri = tokenFetchUri;
+            this.subscriptionKey = subscriptionKey;
+        }
+
+        public async Task<string> FetchTokenAsync()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+                UriBuilder uriBuilder = new UriBuilder(tokenFetchUri);
+
+                HttpResponseMessage result = await client.PostAsync(uriBuilder.Uri.AbsoluteUri, null).ConfigureAwait(false);
+                return await result.Content.ReadAsStringAsync().ConfigureAwait(false);
+            }
+        }
+    }
+}
---
+++ RedditTextToSpeech.Logic/Services/IAzureAuthenticationService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RedditTextToSpeech.Logic.Services
+{
+    internal interface IAzureAuthenticationService
+    {
+        Task<string> FetchTokenAsync();
+    }
+}
