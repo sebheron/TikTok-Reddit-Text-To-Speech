7897d13f858217ec64ccc7409c764fc2729cc591
Seb Heron <sebheron21@hotmail.co.uk>
2022-05-30 19:22:51 +0100

    Return file in image factory

--- RedditTextToSpeech.Logic/Factories/ImageFactory.cs
+++ RedditTextToSpeech.Logic/Factories/ImageFactory.cs
@@ -16,16 +16,16 @@ namespace RedditTextToSpeech.Logic.Factories
 
         public async Task<string> GetImage(string title, string username, string subreddit)
         {
-            var path = $"{Guid.NewGuid().ToString()}";
+            var path = Guid.NewGuid().ToString();
             var file = await this.imageService.GetImage(path, title, username, subreddit);
-            return path;
+            return file;
         }
 
         public async Task<string> GetImage(string text)
         {
-            var path = $"{Guid.NewGuid().ToString()}";
+            var path = Guid.NewGuid().ToString();
             var file = await this.imageService.GetImage(path, text);
-            return path;
+            return file;
         }
     }
 }
