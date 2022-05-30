fded4da52d5e9056c5dbd92992c8a5beaf740655
Seb Heron <sebheron21@hotmail.co.uk>
2022-05-30 12:45:59 +0100

    Add speech service interface

---
+++ RedditTextToSpeech.Logic/Services/ISpeechSynthesisService.cs
@@ -0,0 +1,16 @@
+﻿using RedditTextToSpeech.Core;
+using System.Threading.Tasks;
+
+namespace RedditTextToSpeech.Logic.Services
+{
+    public interface ISpeechSynthesisService
+    {
+        Task<string> GetSound(string path, string voice, string text);
+
+        string[] MaleVoices { get; }
+
+        string[] FemaleVoices { get; }
+
+        string Extension { get; }
+    }
+}
