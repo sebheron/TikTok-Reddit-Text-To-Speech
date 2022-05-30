be354d261eb7bb2c9a16764189f89d35e4d7b0da
Seb Heron <sebheron21@hotmail.co.uk>
2022-05-30 12:47:40 +0100

    Add audio clip and image factory interfaces

---
+++ RedditTextToSpeech.Logic/Factories/IAudioClipFactory.cs
@@ -0,0 +1,11 @@
+﻿using RedditTextToSpeech.Core;
+using System;
+using System.Threading.Tasks;
+
+namespace RedditTextToSpeech.Logic.Factories
+{
+    public interface IAudioClipFactory
+    {
+        Task<FilePath> GetAudioClip(string text, string voice);
+    }
+}
---
+++ RedditTextToSpeech.Logic/Factories/IImageFactory.cs
@@ -0,0 +1,11 @@
+﻿using RedditTextToSpeech.Core;
+using System.Threading.Tasks;
+
+namespace RedditTextToSpeech.Logic.Factories
+{
+    public interface IImageFactory
+    {
+        Task<FilePath> GetImage(string title, string username, string subreddit);
+        Task<FilePath> GetImage(string text);
+    }
+}
