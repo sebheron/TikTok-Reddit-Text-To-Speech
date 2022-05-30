8a4fa1e4b04d3b1334c7c9ce3f5515a1c3523118
Seb Heron <sebheron21@hotmail.co.uk>
2022-05-30 19:22:27 +0100

    Add video factory and implementation

---
+++ RedditTextToSpeech.Logic/Factories/IVideoFactory.cs
@@ -0,0 +1,12 @@
+﻿using RedditTextToSpeech.Core;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RedditTextToSpeech.Logic.Factories
+{
+    public interface IVideoFactory
+    {
+        Task<string> GetVideo(IList<AudioImagePair> values, TimeSpan startTime, string background);
+    }
+}
---
+++ RedditTextToSpeech.Logic/Factories/VideoFactory.cs
@@ -0,0 +1,24 @@
+﻿using RedditTextToSpeech.Core;
+using RedditTextToSpeech.Logic.Services;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RedditTextToSpeech.Logic.Factories
+{
+    internal class VideoFactory : IVideoFactory
+    {
+        private IVideoService videoService;
+        public VideoFactory(IVideoService videoService)
+        {
+            this.videoService = videoService;
+        }
+
+        public async Task<string> GetVideo(IList<AudioImagePair> values, TimeSpan startTime, string background)
+        {
+            var path = Guid.NewGuid().ToString();
+            var file = await this.videoService.GetVideo(path, values, startTime, background);
+            return file;
+        }
+    }
+}
