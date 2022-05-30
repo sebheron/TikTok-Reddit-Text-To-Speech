5ab3b49362b3065922edc2db526c3a079685db8c
Seb Heron <sebheron21@hotmail.co.uk>
2022-05-30 12:47:55 +0100

    Implement image and audio factories

---
+++ RedditTextToSpeech.Logic/Factories/AudioClipFactory.cs
@@ -0,0 +1,24 @@
+﻿using RedditTextToSpeech.Core;
+using RedditTextToSpeech.Logic.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace RedditTextToSpeech.Logic.Factories
+{
+    public class AudioClipFactory : IAudioClipFactory
+    {
+        public ISpeechSynthesisService synthesisService;
+
+        public AudioClipFactory(ISpeechSynthesisService synthesisService)
+        {
+            this.synthesisService = synthesisService;
+        }
+
+        public async Task<FilePath> GetAudioClip(string text, string voice)
+        {
+            var path = $"{Guid.NewGuid().ToString()}";
+            var file = await this.synthesisService.GetSound(path, voice, text);
+            return new FilePath(path);
+        }
+    }
+}
---
+++ RedditTextToSpeech.Logic/Factories/ImageFactory.cs
@@ -0,0 +1,31 @@
+﻿using RedditTextToSpeech.Core;
+using RedditTextToSpeech.Logic.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace RedditTextToSpeech.Logic.Factories
+{
+    public class ImageFactory : IImageFactory
+    {
+        private IImageService imageService;
+
+        public ImageFactory(IImageService imageService)
+        {
+            this.imageService = imageService;
+        }
+
+        public async Task<FilePath> GetImage(string title, string username, string subreddit)
+        {
+            var path = $"{Guid.NewGuid().ToString()}";
+            var file = await this.imageService.GetImage(path, title, username, subreddit);
+            return new FilePath(path);
+        }
+
+        public async Task<FilePath> GetImage(string text)
+        {
+            var path = $"{Guid.NewGuid().ToString()}";
+            var file = await this.imageService.GetImage(path, text);
+            return new FilePath(path);
+        }
+    }
+}
