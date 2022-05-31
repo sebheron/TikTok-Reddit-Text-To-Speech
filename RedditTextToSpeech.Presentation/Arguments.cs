08444d3ddbf967b010b33de7dd432806334d9a13
Seb Heron <sebheron21@hotmail.co.uk>
2022-05-31 23:50:47 +0100

    Add arguments for command line program

---
+++ RedditTextToSpeech.Presentation/Arguments.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PowerArgs;
+using RedditTextToSpeech.Core;
+
+namespace RedditTextToSpeech.Presentation
+{
+    public class Arguments
+    {
+        [ArgRequired(PromptIfMissing = true)]
+        public string Url { get; set; }
+
+        [ArgRequired(PromptIfMissing = true)]
+        public string Background { get; set; }
+
+        public string? Output { get; set; }
+
+        public Gender? Gender { get; set; }
+
+        public TimeSpan? Start { get; set; }
+
+        public int? Comments { get; set; }
+
+        public string? Server { get; set; }
+
+        public string? Key { get; set; }
+    }
+}
